b758999876a01ccd674436656515283912ce29fb
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-16 16:36:26 +0200

    project scaffolding and added possibility to put multiple input in the same line

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// const btn = document.querySelector('button');
+// const inputEl = document.getElementById('data');
+const resultEl = document.querySelector('.alert');
+
+let listaDaAcqiuistare = [];
+let substring = []
+let stop = false;
+
+do {
+    elemento = prompt('inserire gli elementi da acquistare; scrivere "stop" per finire');
+    //if he inserts items with spaces
+    substring = elemento.split(" ");
+    for (let i = 0; i < substring.length; i++) {
+        if (substring[i] !== 'stop') {
+            listaDaAcqiuistare.push(substring[i])
+
+        } else {
+            stop = true;
+            break;
+        }
+    }
+
+} while (!stop);
+
+for (let i = 0; i < listaDaAcqiuistare.length; i++) {
+    console.log(listaDaAcqiuistare[i]);
+    resultEl.innerHTML += `${listaDaAcqiuistare[i]}, `
+}
+
+resultEl.classList.remove('d-none');
